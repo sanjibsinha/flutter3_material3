a1f2514c518ff593bb544a0714a2de54d7514f93
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-06-13 12:26:05 +0530

    theme added to second again

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     final theme = ThemeProvider.of(context);
