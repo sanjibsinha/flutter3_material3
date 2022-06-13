9f74840bba11751174f1502061abfbde955cf552
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-06-13 12:29:14 +0530

    theme added to second again

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:e_commerce_app/models/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -5,15 +7,25 @@ import 'providers/products.dart';
 import 'screens/product_detail_screen.dart';
 import 'screens/products_overview_screen.dart';
 
+final settings = ValueNotifier(
+  ThemeSettings(
+    sourceColor: Colors.yellow.shade900,
+    themeMode: ThemeMode.system,
+  ),
+);
+
 void main() {
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => Products(),
+    ChangeNotifierProvider(
+      create: (context) => Products(),
+      builder: (context, _) => DynamicColorBuilder(
+        builder: (lightDynamic, darkDynamic) => ThemeProvider(
+          lightDynamic: lightDynamic,
+          darkDynamic: darkDynamic,
+          settings: settings,
+          child: const MyApp(),
         ),
-      ],
-      child: const MyApp(),
+      ),
     ),
   );
 }
@@ -21,16 +33,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final theme = ThemeProvider.of(context);
     return MaterialApp(
       title: 'MyShop',
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
-            .copyWith(secondary: Colors.deepOrange),
-      ),
+      theme: theme.dark(settings.value.sourceColor),
       home: const ProductsOverviewScreen(),
       routes: {
         ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
